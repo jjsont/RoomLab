f6ebe76642aac4d9064706f11b1e08f68dea5e68
BT_1N3_29 <jtan5662@bths.edu>
2018-12-05 14:16:37 -0500

    DDDDDD

---
+++ src/Game/Board.java
@@ -0,0 +1,39 @@
+package Game;
+
+import Rooms.Room;
+
+
+public class Board {
+    private Room[][] map;
+    private int length;
+    private int width;
+
+
+    public Board(int length, int width, Room[][] map) {
+        this.length = length;
+        this.width = width;
+        this.map = map;
+    }
+
+    public Board(int length, int width) {
+        this.map = new Room[length][width];
+    }
+
+    public Room[][] getBoard() {
+        return this.map;
+    }
+
+    public void print() {
+        String row= "";
+        for(int i=0; i<map.length; i++) {
+            row= " ";
+            for(int j=0; j<map[i].length; i++) {
+                row+= map[i][j].toString();
+            }
+            System.out.println(row);
+        }
+    }
+
+
+
+}
